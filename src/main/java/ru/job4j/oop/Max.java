6480aa3d2d1cd4bd049b3686f61904a8c7e18897
duckplug <okolelovant@gmail.com>
2021-06-28 20:32:29 +0300

    2.2. Рефакторинг - Площадь треугольника. [#122642 #172429]

---
+++ src/main/java/ru/job4j/oop/Max.java
@@ -0,0 +1,15 @@
+package ru.job4j.oop;
+
+public class Max {
+    public static int max(int left, int right) {
+        return left >= right ? left : right;
+    }
+
+    public static int max(int left, int right, int one) {
+        return max(max(left, right), one);
+    }
+
+    public static int max(int left, int right, int one, int two) {
+        return max(max(left, right, one), two);
+    }
+}
---
+++ src/test/java/ru/job4j/oop/MaxTest.java
@@ -0,0 +1,29 @@
+package ru.job4j.oop;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MaxTest  {
+    @Test
+    public void testNumberThree() {
+            int left = 2;
+            int right = 1;
+            int one = 5;
+            int result = Max.max(left, right, one);
+            int expected = 5;
+            Assert.assertEquals(result, expected);
+        }
+
+    @Test
+    public void testNumberFour() {
+        int left = 2;
+        int right = 1;
+        int one = 5;
+        int two = 9;
+        int result = Max.max(left, right, one, two);
+        int expected = 9;
+        Assert.assertEquals(result, expected);
+    }
+}
+
+
