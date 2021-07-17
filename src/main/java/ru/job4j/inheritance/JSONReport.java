fc76890b39fb7845fedcfeb24c415d9319b99000
duckplug <okolelovant@gmail.com>
2021-07-17 19:50:09 +0300

    4. Переопределение [#174890]

---
+++ src/main/java/ru/job4j/inheritance/JSONReport.java
@@ -0,0 +1,14 @@
+package ru.job4j.inheritance;
+
+public class JSONReport extends TextReport {
+
+    public String generate(String name, String body) {
+        return System.lineSeparator() + "name : " + name + System.lineSeparator() + "body : " + body;
+    }
+
+    public static void main(String[] args) {
+       JSONReport report = new JSONReport();
+        String text = report.generate("name", "body");
+        System.out.println(text);
+    }
+}
