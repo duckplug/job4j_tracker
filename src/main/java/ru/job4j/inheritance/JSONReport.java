3b9b3ebd01d74ab410a9eaca084da0295e03a461
duckplug <okolelovant@gmail.com>
2021-07-19 13:33:08 +0300

    4. Переопределение [#174890 #178728]

--- src/main/java/ru/job4j/inheritance/JSONReport.java
+++ src/main/java/ru/job4j/inheritance/JSONReport.java
@@ -3,7 +3,7 @@ package ru.job4j.inheritance;
 public class JSONReport extends TextReport {
     @Override
     public String generate(String name, String body) {
-        return System.lineSeparator() + "name : " + name + System.lineSeparator() + "body : " + body;
+        return "{" + System.lineSeparator() + "\t " + "\"" + "name" + "\"" + " : " + name + "," + "\n" + "\t " + "\"" + "body" + "\"" + " : " + body + "\n" + "}";
     }
 
     public static void main(String[] args) {
