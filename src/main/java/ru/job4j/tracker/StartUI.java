22856755955a34178601ec21c9d486dccb951852
Anton <okolelovant@gmail.com>
2021-08-05 13:19:30 +0300

    2.6. Реализация класса StartUI. Удаление заявки [#500747]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -24,10 +24,8 @@ public class StartUI {
                         System.out.println(item);
                     }
                 } else {
-                    System.out.println("Хранилище еще не содержит заявок");
+                    System.out.println("Хранилище еще не содержит заявок.");
                 }
-            } else if (select == 6) {
-                run = false;
             } else if (select == 2) {
                 System.out.println("=== Edit item ====");
                 System.out.print("Enter id: ");
@@ -40,6 +38,17 @@ public class StartUI {
                 } else {
                     System.out.println("Ошибка замены заявки.");
                 }
+            } else if (select == 3) {
+                System.out.println("=== Delete item ====");
+                System.out.print("Enter id: ");
+                int id = Integer.parseInt(scanner.nextLine());
+                if (tracker.delete(id)) {
+                    System.out.println("Заявка удалена успешно.");
+                } else {
+                    System.out.println("Ошибка удаления заявки.");
+                }
+            } else if (select == 6) {
+                run = false;
             }
         }
     }
