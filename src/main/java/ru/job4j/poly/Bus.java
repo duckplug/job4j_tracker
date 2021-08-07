29cb38fe757604e0b32ea17bba6381d83429d630
Anton <okolelovant@gmail.com>
2021-08-07 17:02:43 +0300

    3. Интерфейс Input [#181092 #186296]

---
+++ src/main/java/ru/job4j/poly/Bus.java
@@ -0,0 +1,19 @@
+package ru.job4j.poly;
+
+public class Bus implements Transport {
+
+    @Override
+    public void drive() {
+
+    }
+
+    @Override
+    public void pessenger(int number) {
+
+    }
+
+    @Override
+    public int oil(int volume) {
+        return 0;
+    }
+}
---
+++ src/main/java/ru/job4j/poly/Transport.java
@@ -0,0 +1,7 @@
+package ru.job4j.poly;
+
+public interface Transport {
+    String void drive();
+    int void pessenger (int number);
+    int oil (int volume);
+}
