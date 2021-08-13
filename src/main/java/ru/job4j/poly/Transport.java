4590b58f1bd4cbbc8eec15c58508294e0b051ef7
Anton <okolelovant@gmail.com>
2021-08-13 10:01:36 +0300

    3. Интерфейс Input [#181092 #186296]

--- src/main/java/ru/job4j/poly/Transport.java
+++ src/main/java/ru/job4j/poly/Transport.java
@@ -3,7 +3,7 @@ package ru.job4j.poly;
 public interface Transport {
     void drive();
 
-    int void  pessenger(int number, int cost);
+    void  pessenger(int number, int cost);
 
     int oil(int volume, int price);
 }
