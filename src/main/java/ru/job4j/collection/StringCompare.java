4f69540ef2a0e267da92b91aab453111057fc18a
duckplug <okolelovant@gmail.com>
2021-11-19 14:11:42 +0300

    3. Компаратор для строк [#35008 #229554]

--- src/main/java/ru/job4j/collection/StringCompare.java
+++ src/main/java/ru/job4j/collection/StringCompare.java
@@ -8,17 +8,17 @@ public class StringCompare implements Comparator<String> {
     @Override
     public int compare(String left, String right) {
         int rsl = 0;
-        int leng = left.length() >= right.length() ? left.length() : right.length();
-        for (int i = 0; i < leng; i++) {
-            if (Character.isDigit(left.charAt(i))) {
-                rsl = Integer.compare(left.charAt(i), right.charAt(i));
-            } else {
+        if (left.length() != right.length()) {
+            rsl = Integer.compare(left.length(), right.length());
+        } else {
+            for (int i = 0; i < left.length(); i++) {
                 rsl = Character.compare(left.charAt(i), right.charAt(i));
-            }
-            if (rsl != 0) {
-                break;
+                if (rsl != 0) {
+                    break;
+                }
             }
         }
         return rsl;
     }
 }
+
