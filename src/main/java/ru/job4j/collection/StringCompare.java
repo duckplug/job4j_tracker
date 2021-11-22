2335330e3278b99f447ff3f4ad8ef62eda83a946
duckplug <okolelovant@gmail.com>
2021-11-22 15:40:21 +0300

    3. Компаратор для строк [#35008 #229554]

--- src/main/java/ru/job4j/collection/StringCompare.java
+++ src/main/java/ru/job4j/collection/StringCompare.java
@@ -1,24 +1,18 @@
 package ru.job4j.collection;
 
-import java.lang.reflect.Array;
-import java.util.ArrayList;
 import java.util.Comparator;
 
 public class StringCompare implements Comparator<String> {
     @Override
     public int compare(String left, String right) {
         int rsl = 0;
-        if (left.length() != right.length()) {
-            rsl = Integer.compare(left.length(), right.length());
-        } else {
-            for (int i = 0; i < left.length(); i++) {
-                rsl = Character.compare(left.charAt(i), right.charAt(i));
-                if (rsl != 0) {
-                    break;
+        for (int i = 0; i < Math.min(left.length(), right.length()); i++) {
+            rsl = Character.compare(left.charAt(i), right.charAt(i));
+            if (rsl != 0) {
+                   return rsl;
                 }
             }
-        }
-        return rsl;
+        return Integer.compare(left.length(), right.length());
     }
 }
 
--- src/test/java/ru/job4j/collection/StringCompareTest.java
+++ src/test/java/ru/job4j/collection/StringCompareTest.java
@@ -1,7 +1,6 @@
 package ru.job4j.collection;
 
 import org.junit.Test;
-
 import static org.hamcrest.Matchers.greaterThan;
 import static org.hamcrest.Matchers.lessThan;
 import static org.hamcrest.core.Is.is;
