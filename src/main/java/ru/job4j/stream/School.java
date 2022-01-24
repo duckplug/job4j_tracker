bede9123eafc10df1923385876bf17b1d9f957c7
duckplug <okolelovant@gmail.com>
2022-01-24 15:00:35 +0300

    1. Фильтрация учеников [#110058 #253734]

--- src/main/java/ru/job4j/stream/School.java
+++ src/main/java/ru/job4j/stream/School.java
@@ -6,10 +6,9 @@ import java.util.stream.Collectors;
 
 public class School {
     public  List<Student> collect(List<Student> students, Predicate<Student> predict) {
-        List<Student> pullStudents = students.stream().
+        return students.stream().
                 filter(predict).
                 collect(Collectors.toList());
-        return pullStudents;
     }
 }
 
