dc6b1ae1ebb53e592917daca14f0916e635b00ba
duckplug <okolelovant@gmail.com>
2022-02-18 16:18:48 +0300

    3. Optional в банковских переводах. [#214711 #262002] поиск решения метода bestStudent

--- src/main/java/ru/job4j/attestation/trainee/Analyze.java
+++ src/main/java/ru/job4j/attestation/trainee/Analyze.java
@@ -1,29 +1,54 @@
 package ru.job4j.attestation.trainee;
 
+import java.util.Comparator;
+import java.util.LinkedHashMap;
 import java.util.List;
-import java.util.function.Function;
-import java.util.function.ToIntFunction;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Analyze {
 
     public static double averageScore(Stream<Pupil> stream) {
-        return stream.flatMap(i -> i.getSubjects().stream()).mapToInt(Subject::getScore).average().orElse(0D);
+        return stream.flatMap(i -> i.getSubjects()
+                .stream())
+                .mapToInt(Subject::getScore)
+                .average()
+                .orElse(0D);
+    }
 
     public static List<Tuple> averageScoreBySubject(Stream<Pupil> stream) {
-        return stream.map()
+            return stream.map(s -> new Tuple(s.getName(), s.getSubjects()
+                    .stream()
+                    .mapToInt(Subject::getScore)
+                    .average()
+                    .orElse(0D)))
+                    .collect(Collectors.toList());
     }
 
     public static List<Tuple> averageScoreByPupil(Stream<Pupil> stream) {
-        return List.of();
+            return stream.flatMap(s -> s.getSubjects()
+                    .stream())
+                    .collect(Collectors.groupingBy(Subject::getName, LinkedHashMap::new,
+                            Collectors.averagingDouble(Subject::getScore)))
+                    .entrySet()
+                    .stream()
+                    .map(s -> new Tuple(s.getKey(), s.getValue()))
+                    .collect(Collectors.toList());
     }
 
     public static Tuple bestStudent(Stream<Pupil> stream) {
-        return null;
+        return stream.map(s -> new Tuple(s.getName(), s.getSubjects()
+                        .stream()
+                        .mapToInt(Subject::getScore)
+                        .sum()))
+
+
+
+    }
 
 
     public static Tuple bestSubject(Stream<Pupil> stream) {
-        return null;
+            return null;
     }
 }
+
